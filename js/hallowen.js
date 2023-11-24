6790f2985966223b39dbab68be5b91da4624bae0
marialop05 <151635942+marialop05@users.noreply.github.com>
2023-11-24 16:17:55 +0100

    Add files via upload

---
+++ js/hallowen.js
@@ -0,0 +1,21 @@
+/*******************************************
+*
+*
+*
+*
+*/
+
+function show(element){
+    document.getElementById(element).style.visibility="visible"; /*extrae (get) un elemento y cambia algo en su estilo*/
+}
+function hide(element){
+    document.getElementById(element).style.visibility="hidden";
+}
+
+function play(element){
+    document.getElementById(element).play();
+}
+function pause(element){
+    document.getElementById(element).pause();
+}
+
